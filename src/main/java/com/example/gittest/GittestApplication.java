135fc0240df6f633af56015a685e8db266c9c07f
Lang <17618006@qq.com>
2020-08-05 20:26:46 +0800

    测试

--- src/main/java/com/example/gittest/GittestApplication.java
+++ src/main/java/com/example/gittest/GittestApplication.java
@@ -9,6 +9,7 @@ public class GittestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GittestApplication.class, args);
         System.out.println("666");
+        System.out.println("777");
     }
 
 }
